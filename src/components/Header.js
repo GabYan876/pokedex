9f91a1e72b8be012cf12d9ca1a8393a0f224000e
GabYan <gabyan877@gmail.com>
2024-01-05 16:56:17 +0100

    props header - pokemon et type

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,27 @@
-export default function Header({pokemons}) {
+export default function Header({pokemons, types}) {
+
+    // Fonction pour obtenir le nom du type en fonction de l'ID du type
+    const getTypeInfo = typeId => {
+        const type = types.find(t => t.id === typeId);
+        return type ? { name: type.name.en } : null
+    }
+
     return (
         <div className="header">
+            {Array.isArray(pokemons) && pokemons.map(pokemon => (
+                <div key={pokemon.id}>
+                    {pokemon.name.en}
+
+                    {pokemon.types.map(typeId => {
+                    const typeInfo = getTypeInfo(typeId)
+                    return typeInfo ? (
+                        <div key={typeId}>
+                            <h3>{typeInfo.name}</h3>
+                        </div>
+                    ) : null;
+                    })}
+                </div>
+            ))}
         </div>
     )
 }
